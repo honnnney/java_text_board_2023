faa43949e522e7ced726e194f4ab503ba4f554ea
honnnney <tpgjs1023@gmail.com>
2023-02-08 20:36:33 +0900

    exit명령이 나올 때까지 입력받기

--- src/com/ksh/exam/board/Main.java
+++ src/com/ksh/exam/board/Main.java
@@ -8,13 +8,18 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령) ");
+    while (true){
+      System.out.printf("명령) ");
+      String cmd = scanner.nextLine();
 
-    String cmd = scanner.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", cmd);
+      if (cmd.equals("exit")){
+        break;
+      }
+
+      System.out.printf("입력된 명령어 : %s\n", cmd);
+    }
 
     System.out.println("== 프로그램 종료 ==");
-
     scanner.close();
   }
 }
