011364c97202d7d604441d13739a1605f942eb07
honnnney <tpgjs1023@gmail.com>
2023-02-08 20:54:23 +0900

    새 게시물 입력받기 기능 구현

--- src/com/ksh/exam/board/Main.java
+++ src/com/ksh/exam/board/Main.java
@@ -8,15 +8,33 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
+
+    int count = 0;
+
     while (true){
       System.out.printf("명령) ");
       String cmd = scanner.nextLine();
 
-      if (cmd.equals("exit")){
+      count++;
+
+      if (cmd.equals("/usr/article/write")){
+        System.out.println("게시물 등록");
+        System.out.printf("제목 : ");
+        String title = scanner.nextLine();
+        System.out.printf("내용 : ");
+        String body = scanner.nextLine();
+        //System.out.printf("제목 : %s\n", title);
+        //System.out.printf("내용 : %s\n", detail);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", count);
+      }
+
+      else if (cmd.equals("exit")){
         break;
       }
 
-      System.out.printf("입력된 명령어 : %s\n", cmd);
+      else {
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+      }
     }
 
     System.out.println("== 프로그램 종료 ==");
