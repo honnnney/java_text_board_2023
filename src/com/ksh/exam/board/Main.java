20a7e81d36623c35d90793c124b523fce834b185
honnnney <tpgjs1023@gmail.com>
2023-02-20 23:13:57 +0900

    C:/Program Files/Git/usr/article/detail 명령어 사용

--- src/com/ksh/exam/board/Main.java
+++ src/com/ksh/exam/board/Main.java
@@ -12,14 +12,13 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
 
     int articleLastId = 0;
+    Article lastArticle = null;
 
     while (true){
       System.out.printf("명령) ");
       String cmd = scanner.nextLine();
 
       if (cmd.equals("/usr/article/write")){
-
-
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목 : ");
         String title = scanner.nextLine();
@@ -29,17 +28,31 @@ public class Main {
         articleLastId++;
 
         Article article = new Article(id, title, body);
-
-
+        lastArticle = article;
         System.out.println("생성 된 게시물 객체 : " + article);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
 
+      else if (cmd.equals("/usr/article/detail")){
+
+        if (lastArticle == null){
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+        Article article = lastArticle;
+
+
+        System.out.println("- 게시물 상세보기 -");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
+      }
+
+
       else if (cmd.equals("exit")){
         break;
       }
-
       else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
@@ -55,7 +68,7 @@ class Article {
   String body;
   int id;
 
-  Article(int id, String title, String body){
+  public Article(int id, String title, String body){
     this.id = id;
     this.title = title;
     this.body = body;
